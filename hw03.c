c8098be3de4781f0f59729881c550f3b0434971b
jesings <jsinger@stuy.edu>
2018-09-18 11:07:20 -0400

    updated hw03.c

--- hw03.c
+++ hw03.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 int main(){
+    int myArray[10];
+    srand(time(NULL));
+    for(int i = 0;i<10;i++)
+        myArray[i] = rand();
+    int myArray2[10];
+    for(int i = 0;i<10;i++)
+        myArray2[i] = rand();
+
+    
     return 0;
 }
