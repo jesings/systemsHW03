025b25d93a1c440c342fa3df00bb3b5aa3951359
jesings <jsinger@stuy.edu>
2018-09-18 11:44:44 -0400

    updated hw03.c

--- hw03.c
+++ hw03.c
@@ -7,14 +7,14 @@ int main(){
     printf("Array #0:\n");
     for(int i = 0;i<10;i++){
         myArray[i] = rand();
-    	printf("%d, ",myArray[i]);
+    	printf("Element #%d, value: %d \n ",i,myArray[i]);
     }
     printf("\n");
     int myArray2[10];
     printf("Array #1");
     for(int i = 0;i<10;i++){
         *(myArray2+i) = rand();
-        printf("%d, ",*(myArray2+i));
+    	printf("Element #%d, value%d \n ",i,*(myArray2+i));
     }
     printf("\n");
     return 0;
